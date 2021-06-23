ad9668bba9716a197c5acffca3fa5057b33bf7d0
Thyara <thyaranonato@outlook.com>
2021-06-23 18:12:02 -0300

    Adiciona o Exercício #3

---
+++ Fundamentos/bloco_4/dia_1/script3.js
@@ -0,0 +1,11 @@
+let a = 100;
+let b = 50;
+let c = 30;
+
+if (a > b && a > c) {
+  console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+  console.log("O maior número é: " + b + " (b)");
+} else {
+  console.log("O maior número é: " + c + " (c)");
+}
