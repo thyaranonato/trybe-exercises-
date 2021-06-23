e60e837b1564cfbdf522b1ca03cfa9ad781dc5f6
Thyara <thyaranonato@outlook.com>
2021-06-23 16:07:26 -0300

    Adiciona exercícios do Bloco 4 - dia 1

---
+++ Fundamentos/bloco_4/dia_1/script1.js
@@ -0,0 +1,8 @@
+let a = 20;
+let b = 10;
+
+console.log("Soma: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + (a * b));
+console.log("Divisão: " + (a / b));
+console.log("Módulo: " + (a % b));
