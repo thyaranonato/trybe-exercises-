2abb1756c369d85c278925297909b08929d8dd8c
Thyara <thyaranonato@outlook.com>
2021-06-24 18:05:25 -0300

    Adiciona exercícios #6 e #7

---
+++ Fundamentos/bloco_4/dia_2/exercicio_6.js
@@ -0,0 +1,19 @@
+/* 6) Descubra quantos valores ímpares existem no array e 
+imprima o resultado. Caso não exista nenhum, 
+imprima a mensagem: "nenhum valor ímpar encontrado";*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+  if (numbers[index] % 2 !== 0) {
+    resultado += 1;
+  }
+}
+
+if (resultado === 0) {
+  console.log("Nenhum valor ímpar foi encontrado!");
+} else {
+  console.log(resultado);
+}
