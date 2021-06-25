5542f683e91004ba4230d7e7f93cf1c910d358e9
Thyara <thyaranonato@outlook.com>
2021-06-25 18:34:36 -0300

    Adiciona pasta dos exercícios resolvidos no dia 3

---
+++ Fundamentos/bloco_4/dia_3/exercicio_1.js
@@ -0,0 +1,25 @@
+/*1- Para o primeiro exercício de hoje, faça um programa que, 
+dado um valor n qualquer, seja n > 1 , imprima na tela um 
+quadrado feito de asteriscos de lado de tamanho n. 
+Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+*/
+
+let n = 10;
+let simbolo = "*";
+let linhaDeEntrada = "";
+
+for (let index = 0; index < n; index += 1) {
+  linhaDeEntrada = linhaDeEntrada + simbolo;
+}
+
+for (let index = 0; index < n; index += 1) {
+  console.log(linhaDeEntrada);
+}
