771bebd91247f53b57fe1d67d0954f9bfe7f54be
Thyara <thyaranonato@outlook.com>
2021-06-30 13:45:19 -0300

    Adiciona exercícios do conteúdo do dia 1

---
+++ Fundamentos/bloco_5/dia_1/script.js
@@ -0,0 +1,15 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+
+document.getElementsByTagName("div")[0].style.backgroundColor = "orange";
+document.getElementsByTagName("div")[1].style.backgroundColor = "orange";
+
+document.getElementsByTagName("div")[2].style.backgroundColor = "yellow";
+document.getElementsByTagName("div")[3].style.backgroundColor = "yellow";
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple";
+
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+
+document.getElementById("footer-container").style.backgroundColor = "green";
