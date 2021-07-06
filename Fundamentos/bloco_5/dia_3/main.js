81be43928c8f0944f087f836a117b38d0dbf948e
Thyara <thyaranonato@outlook.com>
2021-07-06 14:47:08 -0300

    Adiciona os exercícios do conteúdo resolvidos

--- Fundamentos/bloco_5/dia_3/main.js
+++ Fundamentos/bloco_5/dia_3/main.js
@@ -30,3 +30,40 @@ divUm.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
+
+
+// 1. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// Deve existir apenas um elemento com a classe 'tech'.
+function addClassTech(event) {
+  let techClass = document.querySelector('.tech');
+  techClass.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+divUm.addEventListener('mouseover', addClassTech);
+divDois.addEventListener('mouseover', addClassTech);
+divTres.addEventListener('mouseover', addClassTech);
+
+// 2. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+input.addEventListener('input', function(event) {
+  let techClass = document.querySelector('.tech');
+  techClass.innerText = event.target.value;
+})
+
+// 3. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+  window.location.replace('https://www.linkedin.com/in/thyara-martins-nonato-3716804a/');
+})
+
+// 4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'purple';
+})
+
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+})
