3b92857470713c5f8c526a61c8a372e54ee93814
Thyara <thyaranonato@outlook.com>
2021-07-15 21:16:29 -0300

    Adiciona exercício #3

--- Fundamentos/bloco_6/dia_2/script.js
+++ Fundamentos/bloco_6/dia_2/script.js
@@ -18,7 +18,7 @@ let picker = new Pikaday({
   field: document.getElementById('data-inicio'),
   format: 'DD/MM/YYYY',
   onselect: function() {
-    console.log(this.getMoment().format('DD/MM/YYYY'));
+   this.getMoment().format('DD/MM/YYYY');
   }
 });
 
@@ -33,3 +33,97 @@ function limpar() {
   }
 }
 botaoLimpar.addEventListener("click", limpar);
+
+// Source: https://github.com/horprogs/Just-validate
+new JustValidate('.js-form', {
+  rules: {
+    nome: {
+      required: true,
+      maxLength: 40
+    },
+    email: {
+      required: true,
+      maxLength: 50,
+      email: true
+    },
+    cpf: {
+      required: true,
+      maxLength: 11
+    },
+    endereco: {
+      required: true,
+      maxLength: 200
+    },
+    cidade: {
+      required: true,
+      maxLength: 28
+    },
+    estado: {
+      required: true
+    },
+    radio: {
+      required: true
+    },
+    resumo: {
+      required: true,
+      maxLength: 1000
+    },
+    cargo: {
+      required: true,
+      maxLength: 40
+    },
+    descricao: {
+      required: true,
+      maxLength: 500
+    },
+    data: {
+      required: true
+    },
+  },
+  alert: {
+    nome: {
+    required: 'O campo de nome é obrigatório.',
+    maxLength: 'O limite é de 40 caracteres.'
+    },
+    email: {
+    required: 'O campo de email é obrigatório.',
+    email: 'O email digitado não é válido.',
+    maxLength: 'O limite é de 50 caracteres.'
+    },
+    cpf: {
+    required: 'O campo de CPF é obrigatório.',
+    maxLength: 'O limite é de 11 caracteres.'
+    },
+    endereco: {
+    required: 'O campo endereço é obrigatório.',
+    maxLength: 'O limite é de 200 caracteres.'  
+    },
+    cidade: {
+    required: 'O campo cidade é obrigatório.',
+    maxLength: 'O limite é de 28 caracteres.'
+    },
+    estado: {
+    required: 'O campo estado é obrigatório.',
+    },
+    radio: {
+    required: 'A escolha de um item é obrigatória.',
+    },
+    resumo: {
+    required: 'Este campo é obrigatório.',
+    maxLength: 'O limite é de 1000 caracteres.'
+    },
+    cargo: {
+    required: 'Este campo é obrigatório.',
+    maxLength: 'O limite é de 40 caracteres.'
+    },
+    descricao: {
+    required: 'Este campo é obrigatório.',
+    maxLength: 'O limite é de 500 caracteres.'
+    },
+    data: {
+    required: 'Este campo é obrigatório.',
+    }
+  },
+submitHandler: function (form, values) {
+  console.log(form, values);
+}});
