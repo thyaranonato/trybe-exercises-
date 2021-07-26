054aeefcec7746ef30cbf4c3a304fe56287c3941
Thyara <thyaranonato@outlook.com>
2021-07-26 16:09:12 -0300

    Finaliza exercícios 3 e 4 Parte 1

--- Fundamentos/bloco_7/dia_3/exercise4_p1.js
+++ Fundamentos/bloco_7/dia_3/exercise4_p1.js
@@ -11,15 +11,16 @@ function myFizzBuzz(num) {
 // implemente seus testes aqui
 // Exercício 4 - A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
 // Teste 1 - Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
-
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
 
 // Teste 2 - Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
-
+assert.strictEqual(myFizzBuzz(27), 'fizz');
 
 // Teste 3 - Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
-
+assert.strictEqual(myFizzBuzz(10), 'buzz');
 
 // Teste 4 - Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
-
+assert.strictEqual(myFizzBuzz(22), 22);
 
 // Teste 5 - Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz('20'), false);
