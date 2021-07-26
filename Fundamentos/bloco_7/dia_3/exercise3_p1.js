3477d206fb1f7a5d8e48ac9697f55830da6fe659
Thyara <thyaranonato@outlook.com>
2021-07-26 18:25:02 -0300

    Finaliza exercício 5 Parte 1

--- Fundamentos/bloco_7/dia_3/exercise3_p1.js
+++ Fundamentos/bloco_7/dia_3/exercise3_p1.js
@@ -15,13 +15,13 @@ function myRemoveWithoutCopy(arr, item) {
 // implemente seus testes aqui
 // Exercício 3 - A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
 // Teste 1 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
-//assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
 
 // Teste 2 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-//assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
 // Teste 3 - Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
 
 
 // Teste 4 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
-//assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
--- Fundamentos/bloco_7/dia_3/exercise5_p1.js
+++ Fundamentos/bloco_7/dia_3/exercise5_p1.js
@@ -16,3 +16,9 @@ const obj3 = {
 };
 
 // implemente seus testes aqui
+// Exercício 5 - Compare dois objetos para verificar se são idênticos ou não
+assert.deepStrictEqual(obj1, obj2, 'Erro: os valores dos objetos não são estritamente iguais.');
+
+assert.notDeepStrictEqual(obj1, obj3, 'Erro: os valores dos objetos são estritamente iguais.');
+
+assert.notDeepStrictEqual(obj2, obj3, 'Erro: os valores dos objetos são estritamente iguais.');
