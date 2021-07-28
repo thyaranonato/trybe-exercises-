20db54cb470615ad01dc3b5a9fbd32f46cad7a71
Thyara <thyaranonato@outlook.com>
2021-07-28 18:35:26 -0300

    Adiciona exercício 1

--- Fundamentos/bloco_8/dia_1/paraFixar.js
+++ Fundamentos/bloco_8/dia_1/paraFixar.js
@@ -1,20 +1,11 @@
 // 1 - Crie uma função que retorne a string 'Acordando!!' ;
-const wakeUp = () => {
-  return 'Acordando!!'
-};
-console.log(wakeUp);
+const wakeUp = () => 'Acordando!!';
 
 // 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
-const coffeeTime = () => {
-  return 'Bora tomar café!!'
-};
-console.log(coffeeTime);
+const coffeeTime = () => 'Bora tomar café!!';
 
 // 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
-const sleep = () => {
-  return 'Partiu dormir!!'
-};
-console.log(sleep);
+const sleep = () => 'Partiu dormir!!';
 
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
 const doingThings = (things) => things();
