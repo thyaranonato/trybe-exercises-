652c8acd1b3c284c3f351fda566d6161cdf8f450
Thyara <thyaranonato@outlook.com>
2021-07-30 18:44:58 -0300

    Adiciona exercício 4

--- Fundamentos/bloco_8/dia_3/exercise_4.js
+++ Fundamentos/bloco_8/dia_3/exercise_4.js
@@ -64,43 +64,37 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
-// Exercício 3 -  Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
-// Dica: use a função filter;
+// Exercício 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort.
 
 const expectedResult = [
-  { 
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
   {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
     author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
+    releaseYear: 1951,
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
 ];
 
-function fantasyOrScienceFiction() {
+function oldBooksOrdered() {
   // escreva seu código aqui
-  return books.filter((book) => (book.genre === 'Ficção Científica' || book.genre === 'Fantasia'));
+  return (books.filter((book) => (2021 - book.releaseYear > 60)))
+  .sort((obj1, obj2) => obj1.releaseYear - obj2.releaseYear);
 }
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
