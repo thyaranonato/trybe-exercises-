fee7381276b4494b459beddaa5feac252a07319a
Thyara <thyaranonato@outlook.com>
2021-08-19 18:42:16 -0300

    Add exercicios 2 e 3 em exercise1.test.js

--- Fundamentos/bloco_10/dia_3/test/exercise1.test.js
+++ Fundamentos/bloco_10/dia_3/test/exercise1.test.js
@@ -7,3 +7,37 @@ test('Testando chamada, retorno e quantas vezes a função randonNumber foi cham
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
+
+// Exercício 2 - Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
+test('Testando chamada, retorno e quantas vezes a função randonNumber foi chamada', () => {
+  service.randomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+  expect(service.randomNumber(20, 5)).toBe(4);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(20, 5);
+});
+
+
+// Exercício 3 - Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+describe('Testando implementações', () => {
+  test('Mock função que recebe 3 parâmetros e retorna sua multiplicação', () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+    expect(service.randomNumber(2, 5, 6)).toBe(60);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(2, 5, 6);    
+  });
+
+  test('Mock função que recebe 1 parâmetro e retorna o seu dobro', () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+    service.randomNumber.mockImplementation(a => a * 2);
+    expect(service.randomNumber(10)).toBe(20);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(10);  
+  });
+});
