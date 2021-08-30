b1931cd9969974870ecefeae9de68c69bd3c7439
Thyara <thyaranonato@outlook.com>
2021-08-30 15:45:05 -0300

    Criando o primeiro componente React de classe

---
+++ Front_End/bloco_11/dia_1/para-fixar-app/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.css';
+import Componente from './Component';
+
+function App() {
+  return < Componente />;
+}
+
+export default App;
---
+++ Front_End/bloco_11/dia_1/para-fixar-app/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Componente extends Component {
+  render() {
+    return <div>
+      <h1>Thyara Nonato</h1>
+      <p>Pós-doutoranda em Engenharia Ambiental na UFSC e estudante de desenvolvimento web na Trybe.</p>
+    </div>
+  };
+}; 
+
+export default Componente;
