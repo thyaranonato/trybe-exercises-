79307d43df1e0e90d73c81f2226546034c34ff27
Thyara <thyaranonato@outlook.com>
2021-09-02 15:16:34 -0300

    Refatorando exercício fancy-buttons

--- Front_End/bloco_12/dia_1/fancy-buttons/src/App.js
+++ Front_End/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function handleClick1() {
-  console.log('Clicou no botão 1');
-}
-
-function handleClick2() {
-  console.log('Clicou no botão 2');
-}
-
-function handleClick3() {
-  console.log('Clicou no botão 3');
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+  
+  handleClick1() {
+    console.log(this)
+    console.log('Clicou no botão 1');
+  }
+  
+  handleClick2() {
+    console.log(this)
+    console.log('Clicou no botão 2');
+  }
+  
+  handleClick3() {
+    console.log(this)
+    console.log('Clicou no botão 3');
+  }
   render() {
     return (
       <div>
-      <button onClick={handleClick1}>Botão 1</button>
-      <button onClick={handleClick2}>Botão 2</button>
-      <button onClick={handleClick3}>Botão 3</button>
+      <button onClick={this.handleClick1}>Botão 1</button>
+      <button onClick={this.handleClick2}>Botão 2</button>
+      <button onClick={this.handleClick3}>Botão 3</button>
       </div>
     );
   }
