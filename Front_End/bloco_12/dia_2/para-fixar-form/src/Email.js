48c779b5fdd9dc929bac9115fffb6c727b5a3abc
Thyara <thyaranonato@outlook.com>
2021-09-03 15:34:35 -0300

    Adiciona exercicio para fixar 8

--- Front_End/bloco_12/dia_2/para-fixar-form/src/Email.js
+++ Front_End/bloco_12/dia_2/para-fixar-form/src/Email.js
@@ -3,12 +3,17 @@ import React from 'react';
 class Email extends React.Component{
   render() {
     const { value, handleChange } = this.props;
+
+    let error = undefined
+    if (value !== 'trybe@trybe.com') error = 'Email incorreto!'
+
     return (
       <label>
         Email:
         <input 
           name="email" type="email" onChange={handleChange} value={value}
         />
+        <span>{error ? error : ''}</span>
       </label>
 
     );
--- Front_End/bloco_12/dia_2/para-fixar-form/src/Nome.js
+++ Front_End/bloco_12/dia_2/para-fixar-form/src/Nome.js
@@ -3,10 +3,15 @@ import React from 'react';
 class Nome extends React.Component{
   render() {
     const { value, handleChange } = this.props;
+
+    let error = undefined
+    if (value.length > 30) error = 'Nome muito extenso!'
+
     return (
       <label>
         Nome Completo:
         <input name="nome" type="nome" onChange={handleChange} value={value}/>
+        <span>{error ? error : ''}</span>
       </label>
     );
   }
