39bd364aa0589aafdd1312fafcddf84ecf6dde9b
Thyara <thyaranonato@outlook.com>
2022-01-07 18:30:48 -0300

    Adiciona exercicio 3

--- Back_end/bloco_22/dia_1/my-scripts/imc.js
+++ Back_end/bloco_22/dia_1/my-scripts/imc.js
@@ -1,6 +1,8 @@
+const readline = require('readline-sync');
+
 function calculoIMC() {
-  const peso = 70;
-  const altura = 173;
+  const peso = readline.questionInt('Qual o seu peso? (Em Kg)');
+  const altura = readline.questionInt('Qual a sua altura? (Em cm)');
   console.log(`O seu peso é: ${peso} e a sua altura é: ${altura}`);
   const imc = (peso / Math.pow(altura /100, 2)).toFixed(2);
   console.log(`O seu IMC é: ${imc}`);
