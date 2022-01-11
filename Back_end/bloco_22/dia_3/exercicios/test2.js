8a9f96cd0057c9d735f770f70e33716c8189965a
Thyara <thyaranonato@outlook.com>
2022-01-11 19:06:17 -0300

    Resoluçao exercicio 5

--- Back_end/bloco_22/dia_3/exercicios/test2.js
+++ Back_end/bloco_22/dia_3/exercicios/test2.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 
 const escreveArquivo = require('./escreveArquivo');
 
 describe('Executa a função "escreveArquivo"', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFileSync');
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+
   describe('a resposta', () => {
     it('é uma string', () => {
       const resposta = escreveArquivo('arquivo.txt', 'hello, world!');
