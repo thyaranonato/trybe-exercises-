345e611187986a06a1cfeb32dbd10b8577523ff5
Thyara <thyaranonato@outlook.com>
2022-01-13 15:29:57 -0300

    router middleware

--- Back_end/bloco_22/dia_5/api-receitas/index.js
+++ Back_end/bloco_22/dia_5/api-receitas/index.js
@@ -5,77 +5,23 @@ const authMiddleware = require('./auth-middleware');
 const app = express();
 app.use(bodyParser.json());
 
+// Esta rota não passa pelo middleware de autenticação!
 app.get('/open', function (req, res) {
   res.send('open!')
 });
 
-app.use(authMiddleware);
-
-const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
-];
-
-app.get('/recipes', function (req, res) {
-  res.status(200).json(recipes);
+// Esta rota passa pelo middleware de autenticação!
+app.get('/fechado', authMiddleware, function (req, res) {
+  res.send('closed!')
 });
 
-app.get('/recipes/search', function (req, res) {
-  const { name, maxPrice } = req.query;
-  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice));
-  res.status(200).json(filteredRecipes);
-});
+const recipesRouter = require('./recipesRouter');
 
-app.get('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((r) => r.id === parseInt(id));
-  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+/* Todas as rotas com /recipes/<alguma-coisa> entram aqui e vão para o roteador. */
+app.use('/recipes', recipesRouter);
 
-  res.status(200).json(recipe);
-});
+// app.all('*', function (req, res) {
+//  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+// });
 
-function validateName(req, res, next) {
-  const { name } = req.body;
-  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'});
-
-  next();
-};
-
-app.post('/recipes', validateName, function (req, res) {
-  const { id, name, price, waitTime } = req.body;
-  const { username } = req.user; // Aqui estamos acessando o usuário encontrado no middleware de autenticação.
-  recipes.push({ id, name, price, waitTime, chef: username });
-  res.status(201).json({ message: 'Recipe created successfully!'});
-});
-
-app.put('/recipes/:id', validateName, function (req, res) {
-  const { id } = req.params;
-  const { name, price, waitTime } = req.body;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
-
-  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price, waitTime };
-
-  res.status(204).end();
-});
-
-app.delete('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
-
-  recipes.splice(recipeIndex, 1);
-
-  res.status(204).end();
-});
-
-app.all('*', function (req, res) {
-    return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
-});
-
-app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
-});
+app.listen(3001, () => { console.log('Ouvindo na porta 3001'); });
