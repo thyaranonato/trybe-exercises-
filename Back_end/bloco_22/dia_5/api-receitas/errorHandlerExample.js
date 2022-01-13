2cdac6fa8652a8f70b65ec6b21797ac6c78d1c72
Thyara <thyaranonato@outlook.com>
2022-01-13 15:47:23 -0300

    Utilizando pacote express-rescue

--- Back_end/bloco_22/dia_5/api-receitas/errorHandlerExample.js
+++ Back_end/bloco_22/dia_5/api-receitas/errorHandlerExample.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const fs = require('fs/promises');
 
 const app = express();
 
-app.get('/:fileName', async (req, res, next) => {
-    try {
-        const file = await fs.readFile(`./${req.params.fileName}`);
-        res.send(file.toString('utf-8'));
-    } catch (e) {
-        next(e);
-    }
-});
+app.get(
+  '/:fileName',
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  })
+);
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
