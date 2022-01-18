3d0caed80b1eb15fc903012ad3ec35562e7f4eab
Thyara <thyaranonato@outlook.com>
2022-01-18 16:52:44 -0300

    Resolução exercício 2 do conteúdo

--- Back_end/bloco_23/dia_1/model-example/index.js
+++ Back_end/bloco_23/dia_1/model-example/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 
@@ -7,6 +8,12 @@ app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
+});
+// Exercício 2: Crie uma rota /books para trazer a lista de todos os livros.
+app.get('/books', async (_req, res) => {
+  const books = await Book.getAll();
+
+  res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
