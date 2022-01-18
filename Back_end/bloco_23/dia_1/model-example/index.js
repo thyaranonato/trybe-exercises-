31fc72880eb514cde3c0315a79eed4ce5ea84707
Thyara <thyaranonato@outlook.com>
2022-01-18 17:48:38 -0300

    Criando uma nova pessoa autora

--- Back_end/bloco_23/dia_1/model-example/index.js
+++ Back_end/bloco_23/dia_1/model-example/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Author = require('./models/Author');
 const Book = require('./models/Book');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
@@ -40,6 +42,18 @@ app.get('/books/:id', async (req, res) => {
   if (!book) return res.status(404).json({ message: 'Not found' });
 
   res.status(200).json(book);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 const PORT = process.env.PORT || 3000;
