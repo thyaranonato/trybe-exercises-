662ae3cc2628471286e8b4951462b570ddfa1925
Thyara <thyaranonato@outlook.com>
2022-02-01 11:15:54 -0300

    Impl rota GET /book/:id para pegar livro com id especificado

--- Back_end/bloco_24/dia_1/orm-exercises/controllers/BooksController.js
+++ Back_end/bloco_24/dia_1/orm-exercises/controllers/BooksController.js
@@ -11,6 +11,19 @@ const getAll = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const books = await Book.findByPk(id);
+    res.status(200).json(books);
+  }
+  catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
 module.exports = {
   getAll,
+  getById
 };
--- Back_end/bloco_24/dia_1/orm-exercises/index.js
+++ Back_end/bloco_24/dia_1/orm-exercises/index.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const BooksController = require('./controllers/BooksController');
 
 const app = express();
- app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/books', BooksController.getAll);
+
+app.get('/book/:id', BooksController.getById);
 
  const PORT = 3000;
  app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
