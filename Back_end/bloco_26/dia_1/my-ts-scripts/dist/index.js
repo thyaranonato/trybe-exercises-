a066e92175face0dcae6626a5b01ff74c8165253
Thyara <thyaranonato@outlook.com>
2022-02-17 19:01:36 -0300

    Resolve exercício 02

--- Back_end/bloco_26/dia_1/my-ts-scripts/dist/index.js
+++ Back_end/bloco_26/dia_1/my-ts-scripts/dist/index.js
@@ -1,6 +1,29 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const length_1 = require("./exercise01/length");
-console.log(`10 cm correspondem a: ${(0, length_1.convert)(10, "cm", "m")} m`);
-console.log(`5 km correspondem a: ${(0, length_1.convert)(5, "km", "m")} m`);
-console.log(`20 dam correspondem a: ${(0, length_1.convert)(20, "dam", "mm")} mm`);
+const Comprimento = __importStar(require("./exercise01/length"));
+const Massa = __importStar(require("./exercise02/mass"));
+console.log(`10 cm correspondem a: ${Comprimento.convert(10, "cm", "m")} m`);
+console.log(`5 km correspondem a: ${Comprimento.convert(5, "km", "m")} m`);
+console.log(`20 dam correspondem a: ${Comprimento.convert(20, "dam", "mm")} mm`);
+console.log(`10 cg correspondem a: ${Massa.convert(10, "cg", "g")} g`);
+console.log(`5 kg correspondem a: ${Massa.convert(5, "kg", "g")} g`);
+console.log(`20 dag correspondem a: ${Massa.convert(20, "dag", "mg")} mg`);
--- Back_end/bloco_26/dia_1/my-ts-scripts/index.ts
+++ Back_end/bloco_26/dia_1/my-ts-scripts/index.ts
@@ -1,7 +1,10 @@
-import { convert } from './exercise01/length';
+import * as Comprimento from './exercise01/length';
+import * as Massa from './exercise02/mass';
 
-console.log(`10 cm correspondem a: ${convert(10, "cm", "m")} m`);
+console.log(`10 cm correspondem a: ${Comprimento.convert(10, "cm", "m")} m`);
+console.log(`5 km correspondem a: ${Comprimento.convert(5, "km", "m")} m`);
+console.log(`20 dam correspondem a: ${Comprimento.convert(20, "dam", "mm")} mm`);
 
-console.log(`5 km correspondem a: ${convert(5, "km", "m")} m`);
-
-console.log(`20 dam correspondem a: ${convert(20, "dam", "mm")} mm`);
+console.log(`10 cg correspondem a: ${Massa.convert(10, "cg", "g")} g`);
+console.log(`5 kg correspondem a: ${Massa.convert(5, "kg", "g")} g`);
+console.log(`20 dag correspondem a: ${Massa.convert(20, "dag", "mg")} mg`);
