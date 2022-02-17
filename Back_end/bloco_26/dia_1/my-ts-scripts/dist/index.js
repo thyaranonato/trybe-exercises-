87c3c7f87d9d4ae58b485ab1fd7bad89da0759ec
Thyara <thyaranonato@outlook.com>
2022-02-17 19:05:56 -0300

    Resolve exercício 03

--- Back_end/bloco_26/dia_1/my-ts-scripts/dist/index.js
+++ Back_end/bloco_26/dia_1/my-ts-scripts/dist/index.js
@@ -21,9 +21,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Comprimento = __importStar(require("./exercise01/length"));
 const Massa = __importStar(require("./exercise02/mass"));
+const Volume = __importStar(require("./exercise03/capacity"));
 console.log(`10 cm correspondem a: ${Comprimento.convert(10, "cm", "m")} m`);
 console.log(`5 km correspondem a: ${Comprimento.convert(5, "km", "m")} m`);
 console.log(`20 dam correspondem a: ${Comprimento.convert(20, "dam", "mm")} mm`);
 console.log(`10 cg correspondem a: ${Massa.convert(10, "cg", "g")} g`);
 console.log(`5 kg correspondem a: ${Massa.convert(5, "kg", "g")} g`);
 console.log(`20 dag correspondem a: ${Massa.convert(20, "dag", "mg")} mg`);
+console.log(`10 cl correspondem a: ${Volume.convert(10, "cl", "l")} l`);
+console.log(`5 kl correspondem a: ${Volume.convert(5, "kl", "l")} l`);
+console.log(`20 dal correspondem a: ${Volume.convert(20, "dal", "ml")} ml`);
--- Back_end/bloco_26/dia_1/my-ts-scripts/index.ts
+++ Back_end/bloco_26/dia_1/my-ts-scripts/index.ts
@@ -1,5 +1,6 @@
 import * as Comprimento from './exercise01/length';
 import * as Massa from './exercise02/mass';
+import * as Volume from './exercise03/capacity';
 
 console.log(`10 cm correspondem a: ${Comprimento.convert(10, "cm", "m")} m`);
 console.log(`5 km correspondem a: ${Comprimento.convert(5, "km", "m")} m`);
@@ -8,3 +9,7 @@ console.log(`20 dam correspondem a: ${Comprimento.convert(20, "dam", "mm")} mm`)
 console.log(`10 cg correspondem a: ${Massa.convert(10, "cg", "g")} g`);
 console.log(`5 kg correspondem a: ${Massa.convert(5, "kg", "g")} g`);
 console.log(`20 dag correspondem a: ${Massa.convert(20, "dag", "mg")} mg`);
+
+console.log(`10 cl correspondem a: ${Volume.convert(10, "cl", "l")} l`);
+console.log(`5 kl correspondem a: ${Volume.convert(5, "kl", "l")} l`);
+console.log(`20 dal correspondem a: ${Volume.convert(20, "dal", "ml")} ml`);
