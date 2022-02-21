475535f66729032c5b392906304f2e9f99c2d3d0
Thyara <thyaranonato@outlook.com>
2022-02-21 17:51:42 -0300

    Criando um CRUD de livros

--- Back_end/bloco_26/dia_3/express-typescript/dist/index.js
+++ Back_end/bloco_26/dia_3/express-typescript/dist/index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const books_1 = __importDefault(require("./routes/books"));
 const app = (0, express_1.default)();
 const PORT = 8000;
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Express + TypeScript');
 });
+app.use(books_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- Back_end/bloco_26/dia_3/express-typescript/index.ts
+++ Back_end/bloco_26/dia_3/express-typescript/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
+import BooksRoutes from "./routes/books";
 
 const app = express();
 
 const PORT = 8000;
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript')
 });
+
+app.use(BooksRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
